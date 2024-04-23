0fdd14a55c9a911fefa7a22e6f4c196439f6c6f1
ELSI GPU <cadiou@molcure.io>
2024-04-23 09:36:54 +0900

    init

---
+++ inference.py
@@ -0,0 +1,39 @@
+import click
+import mlflow.projects.env_type
+import pandas as pd
+import numpy as np
+import mlflow
+import mlflow.sklearn
+
+import sklearn
+from sklearn.svm import SVR
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+
+@click.command()
+@click.option("--tracking_uri", default="", type=str)
+@click.option("--run", default="", type=str)
+@click.option("--input", default="", type=str)
+def inference(tracking_uri, run, input):
+    mlflow.set_tracking_uri(tracking_uri)
+    # Extracting Hparams and model's checkpoint from MLflow
+    run_id = run.split('/')[1]
+    run_mlflow = dict(mlflow.get_run(run_id))
+    y_max = float(run['data'].params['y_max'])
+    y_min = float(run['data'].params['y_min'])
+    pipeline = mlflow.pyfunc.load_model(run_mlflow)
+
+    # Inference
+    input = np.fromstring(input, sep=',').reshape(1, -1)
+    y_pred = pipeline.predict(input)
+    y_pred = y_pred*(y_max-y_min) + y_min
+    print("X:", input)
+    print("pred:", y_pred[0])
+
+
+if __name__ == "__main__":
+    inference()
---
+++ train.py
@@ -0,0 +1,72 @@
+import click
+import mlflow.projects.env_type
+import pandas as pd
+import numpy as np
+import mlflow
+import mlflow.sklearn
+
+import sklearn
+from sklearn.svm import SVR
+from sklearn.datasets import load_diabetes
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+
+@click.command()
+@click.option("--tracking_uri", default="", type=str)
+@click.option("--call", default="", type=str)
+@click.option("--input", default="", type=str)
+def train(tracking_uri, call, input):
+    mlflow.set_tracking_uri(tracking_uri)
+    # We create the remote experiment (on the server, if it doesn't already exist)
+    experiment = mlflow.set_experiment("test-sklearn")
+    # We create a run locally with the same experiment id as the one on the server
+    client = mlflow.tracking.MlflowClient()
+    run = client.create_run(experiment.experiment_id)
+    with mlflow.start_run(run_id = run.info.run_id):
+        # # Log artifacts
+        # mlflow.log_artifact("conda.yaml")
+        # mlflow.log_artifact("Dockerfile")
+        # mlflow.log_artifact("MLproject")
+        # mlflow.log_artifact("project.py")
+        # print("coucou")
+        # exit()
+        # Loading dataset
+        dia_df = load_diabetes()
+        X = dia_df.data
+        y = dia_df.target
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+
+        # Normalizing y
+        y_min = y_train.min()
+        y_max = y_train.max()
+        y_train = (y_max-y_train)/(y_max-y_min)
+        y_test = (y_max-y_test)/(y_max-y_min)
+
+        # Creating a pipeline
+        pipeline = Pipeline([
+            ('scaler', MinMaxScaler()),
+            ('model', SVR()),
+        ])
+
+        # Training model
+        pipeline.fit(X_train, y_train)
+
+        # Evaluating model
+        y_pred = pipeline.predict(X_test)
+        y_pred = y_pred*(y_max - y_min) + y_min
+        y_test = y_test*(y_max - y_min) + y_min
+        rmse = np.sqrt(((y_test - y_pred)**2).mean())
+
+        # Log materials to MLflow
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_params({
+            "y_min": y_min,
+            "y_max": y_max
+        })
+        mlflow.sklearn.log_model(pipeline, artifact_path="model")
+
+if __name__ == "__main__":
+    train()
